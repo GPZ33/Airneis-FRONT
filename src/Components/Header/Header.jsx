8292d983ac977d680a0911bf8f091b8cdbc38289
MAXIME Théo <theomax13@hotmail.fr>
2024-06-04 12:27:19 +0200

    fix: charte graphique

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,94 +1,134 @@
 import logo from "../Assets/logo.png";
-import {Link, useNavigate} from "react-router-dom";
-import React, {useContext, useEffect, useState} from "react";
+import { Link, useNavigate } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
 import "./Header.css";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import SearchIcon from '@mui/icons-material/Search';
-import {useCart} from "../../Context/CartContext";
+import SearchIcon from "@mui/icons-material/Search";
+import { useCart } from "../../Context/CartContext";
 
+const Header = ({ isAuthenticated, setIsAuthenticated }) => {
+  const { cart } = useCart();
+  const navigate = useNavigate();
+  const totalQuantity = cart.reduce((sum, item) => sum + item.quantity, 0);
+  const [collapsedMenu, setCollapsedMenu] = useState(true);
 
-const Header = ({isAuthenticated, setIsAuthenticated}) => {
-    const { cart } = useCart();
-    const navigate = useNavigate();
-    const totalQuantity = cart.reduce((sum, item) => sum + item.quantity, 0)
-    const [collapsedMenu, setCollapsedMenu] = useState(true);
+  const handleToggleCollapse = () => {
+    if (collapsedMenu) {
+      setCollapsedMenu(false);
+    } else {
+      setCollapsedMenu(true);
+    }
+  };
 
-    const handleToggleCollapse = () => {
-        if (collapsedMenu) {
-            setCollapsedMenu(false);
-        } else {
-            setCollapsedMenu(true)
-        }
-    };
+  const handleLogout = () => {
+    setIsAuthenticated(false);
+    localStorage.removeItem("token");
+    localStorage.removeItem("isAuthenticated");
+    setCollapsedMenu(true);
+    navigate("/log_in");
+  };
+  useEffect(() => {
+    const storedAuthStatus = localStorage.getItem("isAuthenticated");
+    if (storedAuthStatus) {
+      setIsAuthenticated(true);
+    }
+  }, []);
+  return (
+    <nav className="navbar">
+      <div className="container-fluid">
+        <div className="align-content-start d-flex justify-content-center align-items-center">
+          <img className="menu-image p-1" src={logo} alt="logo" />
+          <a className="navbar-brand text-white" href="/">
+            ÀIRNEIS
+          </a>
+        </div>
 
+        <div className="align-content-end d-flex">
+          <Link className="flex-end px-1" to="/search">
+            <SearchIcon sx={{ color: "#FFC900", fontSize: 40 }} />
+          </Link>
+          <Link className="flex-end px-1" to="/cart">
+            <ShoppingCartIcon sx={{ color: "#FFC900", fontSize: 40 }} />
+            {totalQuantity > 0 && (
+              <span className="cart-count">{totalQuantity}</span>
+            )}
+          </Link>
 
-    const handleLogout = () => {
-        setIsAuthenticated(false);
-        localStorage.removeItem("token");
-        localStorage.removeItem("isAuthenticated");
-        setCollapsedMenu(true);
-        navigate("/log_in");
-    };
-    useEffect(() => {
-        const storedAuthStatus = localStorage.getItem("isAuthenticated");
-        if (storedAuthStatus) {
-            setIsAuthenticated(true);
-        }
-    }, []);
-    return (
-        <nav className="navbar navbar-light bg-light">
-            <div className="container-fluid">
-                <div className="align-content-start d-flex justify-content-center align-items-center">
-                    <img className="menu-image p-1" src={logo} alt="logo"/>
-                    <a className="navbar-brand" href="/">ÀIRNEIS</a>
-                </div>
-
-                <div className="align-content-end d-flex">
-                    <Link className="flex-end px-1" to="/search"><SearchIcon
-                        sx={{color: "#151D53", fontSize: 40}}/></Link>
-                    <Link className="flex-end px-1" to="/cart">
-                        <ShoppingCartIcon sx={{color: "#151D53", fontSize: 40}}/>
-                        {totalQuantity > 0 && (
-                            <span className="cart-count">{totalQuantity}</span>
-                        )}
-                    </Link>
-
-                    <button className="navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
-                            aria-label="Toggle navigation" >
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className={`collapse navbar-collapse ${collapsedMenu ? "" : "show"}`} onClick={handleToggleCollapse} id="navbarNavAltMarkup">
-                        <div className="navbar-nav ml-auto">
-                            {isAuthenticated ? (
-                                <>
-                                    <Link className="nav-link" to="/user_settings" onClick={handleToggleCollapse}>Mes paramètres</Link>
-                                    <Link className="nav-link" to="/orders">Mes commandes</Link>
-                                    <Link className="nav-link" to="/CGU">CGU</Link>
-                                    <Link className="nav-link" to="/legal_notice">Mentions légales</Link>
-                                    <Link className="nav-link" to="/contact">Contact</Link>
-                                    <Link className="nav-link" to="/about_us">A propos d'AIRNEIS</Link>
-                                    <button className="nav-link" onClick={handleLogout}>Se déconnecter</button>
-                                </>
-                            ) : (
-                                <>
-                                    <Link className="nav-link" to="/log_in">Se connecter</Link>
-                                    <Link className="nav-link" to="/sign_up">S'inscrire</Link>
-                                    <Link className="nav-link" to="/CGU">CGU</Link>
-                                    <Link className="nav-link" to="/legal_notice">Mentions légales</Link>
-                                    <Link className="nav-link" to="/contact">Contact</Link>
-                                    <Link className="nav-link" to="/about_us">A propos d'AIRNEIS</Link>
-                                </>
-                            )}
-                        </div>
-                    </div>
-                </div>
-
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className={`collapse navbar-collapse ${
+              collapsedMenu ? "" : "show"
+            }`}
+            onClick={handleToggleCollapse}
+            id="navbarNavAltMarkup"
+          >
+            <div className="navbar-nav ml-auto">
+              {isAuthenticated ? (
+                <>
+                  <Link
+                    className="nav-link"
+                    to="/user_settings"
+                    onClick={handleToggleCollapse}
+                  >
+                    Mes paramètres
+                  </Link>
+                  <Link className="nav-link" to="/orders">
+                    Mes commandes
+                  </Link>
+                  <Link className="nav-link" to="/CGU">
+                    CGU
+                  </Link>
+                  <Link className="nav-link" to="/legal_notice">
+                    Mentions légales
+                  </Link>
+                  <Link className="nav-link" to="/contact">
+                    Contact
+                  </Link>
+                  <Link className="nav-link" to="/about_us">
+                    A propos d'AIRNEIS
+                  </Link>
+                  <button className="nav-link" onClick={handleLogout}>
+                    Se déconnecter
+                  </button>
+                </>
+              ) : (
+                <>
+                  <Link className="nav-link" to="/log_in">
+                    Se connecter
+                  </Link>
+                  <Link className="nav-link" to="/sign_up">
+                    S'inscrire
+                  </Link>
+                  <Link className="nav-link" to="/CGU">
+                    CGU
+                  </Link>
+                  <Link className="nav-link" to="/legal_notice">
+                    Mentions légales
+                  </Link>
+                  <Link className="nav-link" to="/contact">
+                    Contact
+                  </Link>
+                  <Link className="nav-link" to="/about_us">
+                    A propos d'AIRNEIS
+                  </Link>
+                </>
+              )}
             </div>
-        </nav>
-
-
-    );
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
